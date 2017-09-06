0224a5db6a1150265199255ae1c3118ac7bae8c9
Ben Botto <benjamin.botto@gmail.com>
2017-09-05 18:07:06 -0700

    Policy evalutation for Blackjack working.

--- blackjack/Blackjack.py
+++ blackjack/Blackjack.py
@@ -1,12 +1,13 @@
-import numpy as np
 from random import randint;
-from enum import IntEnum
 
 class Blackjack:
   STAY = 0
   HIT  = 1
 
   def __init__(self):
+    # Becomes True when the game is over.
+    self.done = False
+
     # A deck of cards.
     self.deck = []
 
@@ -31,8 +32,9 @@ class Blackjack:
   def getRandomCard(self):
     return self.deck[randint(0, len(self.deck) - 1)]
 
-  # Reset the game.
+  # Reset the game and return the initial state (see step()).
   def reset(self):
+    self.done        = False
     self.dealerCards = [self.getRandomCard(), self.getRandomCard()]
     self.playerCards = [self.getRandomCard(), self.getRandomCard()]
 
@@ -40,6 +42,11 @@ class Blackjack:
     # player should always hit because it's not possible to bust).
     while self.getPlayerScore() < 12:
       self.hitPlayer()
+
+    return [
+      self.getPlayerScore(),
+      self.getDealerVisibleScore(),
+      1 if self.playerHasUsableAce() else 0]
 
   # Sum up the score of cards.  If countUsableAce is False, just the sum of the
   # cards, not accounting for usable aces, otherwise add 10 if the cards have a
@@ -50,7 +57,7 @@ class Blackjack:
     for card in cards:
       score += card
 
-    if countUsableAce and self.playerHasUsableAce():
+    if countUsableAce and self._hasUsableAce(cards):
       score += 10
 
     return score
@@ -85,4 +92,70 @@ class Blackjack:
   # Give the dealer another card.
   def hitDealer(self):
     self.dealerCards.append(self.getRandomCard())
+
+  # Get the player's cards.
+  def getPlayerCards(self):
+    return self.playerCards
+
+  # Get the dealer's cards.
+  def getDealerCards(self):
+    return self.dealerCards
+
+  # Get the dealer's visible card value, 1-10 (i.e. excluding the first card).
+  def getDealerVisibleScore(self):
+    return self.dealerCards[1]
+
+  # Check if the player is bust.
+  def playerIsBust(self):
+    return self.getPlayerScore() > 21
+
+  # Check if the dealer is bust.
+  def dealerIsBust(self):
+    return self.getDealerScore() > 21
+
+  # Step the environment (hit or stay) and return the new state, the reward (if
+  # any), and a flag indicating whether or not the game is done.  The state is
+  # a numpy array containing the player score, the dealer's one visible card,
+  # and whether or not the player has an ace (0 or 1).
+  def step(self, action):
+    if self.done:
+      raise RuntimeException('step() called but the game has ended.')
+
+    if action == Blackjack.HIT:
+      self.hitPlayer()
+
+      # If the player went bust, the game is over and the player loses.
+      if self.playerIsBust():
+        self.done = True
+        reward    = -1
+      else:
+        # No reward yet because the game isn't done.
+        reward = 0
+    else:
+      # Player decided to stay.
+      self.done = True
+
+      # The dealer sticks if his score is 17 or more.
+      while self.getDealerScore() < 17:
+        self.hitDealer()
+
+      # If the dealer went bust then the game is over and the player wins.
+      if self.dealerIsBust():
+        reward = 1
+      else:
+        # Reward is based on the scores.
+        pScore = self.getPlayerScore()
+        dScore = self.getDealerScore()
+
+        if pScore == dScore:
+          reward = 0
+        elif pScore < dScore:
+          reward = -1
+        else:
+          reward = 1
+
+    return [
+      self.getPlayerScore(),
+      self.getDealerVisibleScore(),
+      1 if self.playerHasUsableAce() else 0], reward, self.done
 
--- blackjack/blackjack-game.py
+++ blackjack/blackjack-game.py
@@ -1,13 +1,76 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
 from Blackjack import Blackjack
 
 # How many times to play the game.
-NUM_EPISODES = 5
+NUM_EPISODES = 500000
+
+NUM_PLAYER_SCORES = 10 # 12 - 21
+NUM_DEALER_CARDS  = 10 # ace - 10
+NUM_USABLE_ACE    = 2  # 0 or 1.
+NUM_STATES        = NUM_PLAYER_SCORES * NUM_DEALER_CARDS * NUM_USABLE_ACE
+
+# This will get populated with the average returns for each state.  The first
+# index is the player's score, 12-21, the second is the dealer's visible card
+# (ace-10), and the third is whether or not the player has a usable ace (0 or
+# 1).  Total of 200 states.
+values = [0 for _ in range(NUM_STATES)]
+
+# A list of the returns for each state.
+returns = [[] for _ in range(NUM_STATES)]
+
+# Helper function to get the index of a state in the values or returns arrays.
+def getIndex(pScore, dVisScore, hasVisibleAce):
+  # 12 is the lowest score the player can have, and corresponds to index 0.
+  # 1 is the lowest visible card the dealer can have, and corresponds to index 0.
+  return (pScore - 12) * (NUM_DEALER_CARDS * NUM_USABLE_ACE) + (dVisScore - 1) * (NUM_USABLE_ACE) + hasVisibleAce
 
 env = Blackjack()
 
-print(Blackjack.HIT)
+for episode in range(NUM_EPISODES):
+  # Reset the environment.
+  obs  = env.reset()
+  done = False
 
-#for episode in range(NUM_EPISODES):
-#  env.reset()
+  # This array is used to keep track of the states that are seen in an single
+  # episode.
+  episodeStateIndices = [getIndex(obs[0], obs[1], obs[2])]
 
+  print('Episode {}.'.format(episode))
 
+  while not done:
+    # Player policy is to hit if the score is less than 20.
+    action = Blackjack.HIT if obs[0] < 20 else Blackjack.STAY
+    obs, reward, done = env.step(action)
+
+    if obs[0] <= 21:
+      episodeStateIndices.append(getIndex(obs[0], obs[1], obs[2]))
+
+  # For each state seen, add the new reward and update the average value.
+  for ind in episodeStateIndices:
+    returns[ind].append(reward)
+    values[ind] = sum(returns[ind]) / len(returns[ind])
+
+# Plot the results.
+X, Y = np.meshgrid(np.arange(12, 22), np.arange(1, 11), indexing='ij')
+Z_nousable = []
+Z_usable   = []
+
+for p in range(NUM_PLAYER_SCORES):
+  Z_nousable.append([])
+  Z_usable.append([])
+
+  for d in range(NUM_DEALER_CARDS):
+    ind = p * (NUM_DEALER_CARDS * NUM_USABLE_ACE) + d * (NUM_USABLE_ACE)
+    Z_nousable[p].append(values[ind])
+    Z_usable[p].append(values[ind+1])
+
+fig = plt.figure('Without Usable Ace')
+ax = fig.gca(projection='3d')
+surf = ax.plot_surface(X, Y, Z_nousable)
+
+fig = plt.figure('With Usable Ace')
+ax = fig.gca(projection='3d')
+surf = ax.plot_surface(X, Y, Z_usable)
+plt.show()
